ac74faf0e863aaaa47ca3f5c7134acd8f2e9fb13
Edesiri <ohwomadodezri@gmail.com>
2023-04-04 21:50:41 +0100

    Registration Serialization

--- Remcruit_Fullstack/Backend/authentication/api/views.py
+++ Remcruit_Fullstack/Backend/authentication/api/views.py
@@ -1,30 +1,78 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-from .serializers import JobSeekerRegisterSerializer, UserSerializer, EmployerRegisterSerializer
+from .serializers import *
+
+from rest_framework.decorators import api_view
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.views import APIView
+from rest_framework import status
+from .permissions import IsEmployerUser, IsJobSeekerUser
+from rest_framework.permissions import IsAuthenticated
 
 
-class EmployerRegisterView(generics.GenericAPIView):
-    serializer_class=EmployerRegisterSerializer
+@api_view(['POST',])
+def JobSeekerRegisterView(request):
+    if request.method == 'POST':
+        serializer = JobSeekerRegisterSerializer(data=request.data)
+        data = {}
+
+        if serializer.is_valid():
+            jobseeker = serializer.save()
+            data['response'] = "You have been successfully registered"
+            data['email'] = jobseeker.user.email
+            data['username'] = jobseeker.user.username
+            return Response(data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST',])
+def EmployerRegisterView(request):
+    if request.method == 'POST':
+        serializer = EmployerRegisterSerializer(data=request.data)
+        data = {}
+
+        if serializer.is_valid():
+            employer = serializer.save()
+            data['response'] = "You have been successfully registered"
+            data['email'] = employer.user.email
+            data['username'] = employer.user.username
+            return Response(data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginAuthToken(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
-        serializer=self.get_serializer(data=request.data)
+        serializer = self.serializer_class(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
-        user=serializer.save()
+        user = serializer.validated_data['user']
+        token, created= Token.objects.get_or_create(user=user)
         return Response({
-            "user":UserSerializer(user, context=self.get_serializer_context()).data,
-            "token": Token.objects.get(user=user).key,
-            "message":"account has been successfully created"
+            'token':token.key,
+            'user_id': user.pk,
+            'email': user.email,
+            "is_jobSeeker": user.is_jobSeeker,
+            "is_employer": user.is_employer
         })
     
+class LogoutView(APIView):
+    def post(self, request, format=None):
+        request.auth.delete()
+        return Response(status=status.HTTP_200_OK)
+    
+class EmployerOnlyView(generics.RetrieveAPIView):
+    permission_classes = [permissions.IsAuthenticated&IsEmployerUser]
+    serializer_class = UserSerializer
 
-class JobSeekerRegisterView(generics.GenericAPIView):
-    serializer_class=JobSeekerRegisterSerializer
-    def post(self, request, *args, **kwargs):
-        serializer=self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user=serializer.save()
-        return Response({
-            "user":UserSerializer(user, context=self.get_serializer_context()).data,
-            "token": Token.objects.get(user=user).key,
-            "message":"account has been successfully created"
-        })
+    def get_object(self):
+        return self.request.user
+    
+class JobSeekerOnlyView(generics.RetrieveAPIView):
+    permission_classes = [permissions.IsAuthenticated&IsJobSeekerUser]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+    
+    # 
