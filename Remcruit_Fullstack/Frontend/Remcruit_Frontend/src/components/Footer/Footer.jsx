b06f407d9cb74b772db2938d8daf766f92eb7a94
Maryam-Titilope <titilopeasunmo1234@gmail.com>
2023-04-15 22:33:15 +0100

    pulling for back end

--- Remcruit_Fullstack/Frontend/Remcruit_Frontend/src/components/Footer/Footer.jsx
+++ Remcruit_Fullstack/Frontend/Remcruit_Frontend/src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './Footer.css';
 import Logo from '../../assets/footerlogo.svg'
-import { useMediaQuery } from 'react-responsive'
+
 
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
