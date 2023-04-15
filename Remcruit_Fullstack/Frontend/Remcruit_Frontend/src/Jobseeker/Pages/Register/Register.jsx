2432f38e3ff68c1be8d007f4aaa8b4faba46fdf2
Maryam-Titilope <titilopeasunmo1234@gmail.com>
2023-04-15 13:01:41 +0100

    pushing change password backend

--- Remcruit_Fullstack/Frontend/Remcruit_Frontend/src/Jobseeker/Pages/Register/Register.jsx
+++ Remcruit_Fullstack/Frontend/Remcruit_Frontend/src/Jobseeker/Pages/Register/Register.jsx
@@ -2,22 +2,36 @@ import React from "react";
 import "../../../pages/Login/Login.css";
 import Carousel from "../../../pages/components/Carousel/Carousel";
 import RegisterForm from "../../../pages/components/RegisterForm/RegisterForm";
+import ExternalLogin from '../../../components/ExternalLogin/ExternalLogin'
 
 function Register() {
   return (
-    <div>
-      <div className="login-main-container">
+    <div> 
+      <div className="register-main-container">
         <div className="right-container">
           <div className="right-container-content">
             <h1>Create your account</h1>
-            <RegisterForm />
+
+            <div className="external">
+            <ExternalLogin/>
+            </div>
+
+
+            <div className="horizontal-section">
+              <p className="hr-lines">or</p>
+            </div>
+
+
+            <p className="continuewithemail"><Link to="/RegisterPage2">Continue with email</Link></p>
+        <input type="submit"/>
+        <p className="login"><Link to="/login">Already have an account? Login</Link></p>
+
           </div>
         </div>
         <div className="left-container">
           <div className="left-text">
             <p>
-              “Remcruit helped me land my dream job in the tech
-              industry.........”
+            “I found finding a job at Remcruit one of the easiest processes ive ever had to deal with”
             </p>
           </div>
           <div className="carousel">
