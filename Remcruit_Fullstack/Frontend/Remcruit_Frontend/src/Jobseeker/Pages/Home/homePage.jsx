4fa77c954becb6eceb814f4102c846387acfc92a
Edesiri <ohwomadodezri@gmail.com>
2023-04-15 21:39:26 +0100

    Fixingss

--- Remcruit_Fullstack/Frontend/Remcruit_Frontend/src/Jobseeker/Pages/Home/homePage.jsx
+++ Remcruit_Fullstack/Frontend/Remcruit_Frontend/src/Jobseeker/Pages/Home/homePage.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import './homePageStyles.css'
 import JobBox from '../../Components/jobBoxComponent/jobBox'
+import Footer from '../../../components/Footer/Footer'
+import AuthContext from '../../../context/AuthContext'
 
 function HomePage() {
+    let {user} = useContext(AuthContext)
     return (
         <div>
-            {/* {user && <p>Hello {user.username}</p>} */}
+            <div className="homepage-content">
+            {user && <p>Hello {user.username}</p>}
             homePage
             <JobBox />
+            </div>
+            
+            <Footer/>
         </div>
     )
 }
