23abf92218022557d912600a715a1c226891b51d
Edesiri <ohwomadodezri@gmail.com>
2023-05-01 22:01:02 +0100

    Fixing Ruka's Mess on my page

--- Remcruit_Fullstack/Frontend/Remcruit_Frontend/src/utils/EmployerRoute.jsx
+++ Remcruit_Fullstack/Frontend/Remcruit_Frontend/src/utils/EmployerRoute.jsx
@@ -1,12 +1,12 @@
-import React, { useContext } from 'react'
-import AuthContext from '../context/AuthContext'
-import { Navigate, Outlet, useLocation } from 'react-router'
+import React, { useContext } from "react";
+import AuthContext from "../context/AuthContext";
+import { Navigate, Outlet, useLocation } from "react-router";
 
 function EmployerRoute() {
-    let {user} = useContext(AuthContext)
-    let {company} = useContext(AuthContext)
-    const location = useLocation()
-  return user&&company ? <Outlet/> : <Navigate to='/home' state={{from:location}} replace/>
+  let { user } = useContext(AuthContext);
+  let { company } = useContext(AuthContext);
+  const location = useLocation();
+  return user && company ? <Outlet /> : <Navigate to="/home" />;
 }
 
-export default EmployerRoute
+export default EmployerRoute;
